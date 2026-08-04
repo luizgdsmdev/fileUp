ed55d80f400e29aee408571025a07ed223ec21b4
Luiz Messias <luizgdsmdev@gmail.com>
2026-08-04 08:32:28 -0300

    add(basic spring arch): adding basic structure for the java backend using springBoot

---
+++ FileUp/src/test/java/com/bytebybyte/fileup/FileUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebybyte.fileup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
